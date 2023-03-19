9ac2028912a7ba4fc7d20324f5136940a0740649
ghmorrison <19512091+ghmorrison@users.noreply.github.com>
2023-03-19 15:37:55 -0500

    finished homework

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(..., defaultData = matrix(), calc.funct = function(theData, ...) solve(theData)) {
+  if(length(list(...)) > 0){
+    myData <- list(...)[[1]]
+  } else {
+    myData <- defaultData
+  }
+  calcValue <- NULL
+  reset <- function(newData) {
+    myData <<- newData
+    calcValue <<- NULL
+  }
+  calc.value <- function(...)calcValue <<- calc.funct(myData, ...)
+  check.value <- function() calcValue
+  list(reset = reset,
+       check.value = check.value,
+       calc.value = calc.value)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
+  calcValue <- x$check.value()
+  if(!is.null(calcValue)) {
+    message("getting cached data")
+    return(calcValue)
+  }
+  calcValue <- x$calc.value(...)
+  calcValue
         ## Return a matrix that is the inverse of 'x'
 }
+fred <- matrix(c(1, 4 , 9, 5), ncol = 2)
+joe <- makeCacheMatrix()
+cacheSolve(joe)
+cacheSolve(joe)
